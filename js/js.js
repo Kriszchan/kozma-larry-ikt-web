9596642ae711f2911011a22376aae712b1bef68c
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2023-10-17 16:21:43 +0200

    larry_content

---
+++ js/js.js
@@ -0,0 +1,64 @@
+function div1Show() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    let div3 = document.getElementById("div3");
+    let div4 = document.getElementById("div4");
+    let div5 = document.getElementById("div5");
+    div1.style.display = "block";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+  }
+  
+  function div2Show() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    let div3 = document.getElementById("div3");
+    let div4 = document.getElementById("div4");
+    let div5 = document.getElementById("div5");
+    div1.style.display = "none";
+    div2.style.display = "block";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "none";
+  }
+  
+  function div3Show() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    let div3 = document.getElementById("div3");
+    let div4 = document.getElementById("div4");
+    let div5 = document.getElementById("div5");
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "block";
+    div4.style.display = "none";
+    div5.style.display = "none";
+  }
+  
+  function div4Show() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    let div3 = document.getElementById("div3");
+    let div4 = document.getElementById("div4");
+    let div5 = document.getElementById("div5");
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "block";
+    div5.style.display = "none";
+  }
+
+  function div5Show() {
+    let div1 = document.getElementById("div1");
+    let div2 = document.getElementById("div2");
+    let div3 = document.getElementById("div3");
+    let div4 = document.getElementById("div4");
+    let div5 = document.getElementById("div5");
+    div1.style.display = "none";
+    div2.style.display = "none";
+    div3.style.display = "none";
+    div4.style.display = "none";
+    div5.style.display = "block";
+  }
