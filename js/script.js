0aaeb0c5269aa05c7df417ea7bb174c97a8d1103
Kriszchan <panyik.krisztian@students.jedlik.eu>
2023-10-23 20:31:00 +0200

    asd

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function wchange(valtozando, wid)
             {
                 document.getElementById(valtozando).style.width =  wid;
             }
-            function change(megjelen = '')
+function change(megjelen = '')
             {
                 if(megjelen == 'kozma')
                 {
@@ -14,34 +14,30 @@ function wchange(valtozando, wid)
                     document.getElementsByClassName('kozma')[0].style.display = "none";
                     document.getElementsByClassName('larry')[0].style.display = "block";
                 }
-                document.getElementsByClassName('box')[0].style.width = "50vw";
-                document.getElementsByClassName('box')[1].style.width = "50vw";
-                document.getElementsByClassName('container_jobb')[0].style.position = "absolute";
-                document.getElementsByClassName('container_bal')[0].style.position = "absolute";
-                document.getElementsByClassName('container_jobb')[0].style.width = "50vw";
-                document.getElementsByClassName('container_bal')[0].style.width = "50vw";
-                document.getElementsByClassName('container_jobb')[0].style.width = "0";
-                document.getElementsByClassName('container_bal')[0].style.width = "0";
-                document.getElementsByClassName('container_jobb')[0].style.bottom = "0";
-                document.getElementsByClassName('container_bal')[0].style.bottom = "0";
-                document.getElementsByClassName('container_jobb')[0].style.left = "0";
-                document.getElementsByClassName('container_bal')[0].style.right = "0";
-                document.getElementsByClassName('container_jobb')[0].style.height = "100%";
-                document.getElementsByClassName('container_bal')[0].style.height = "100%";
+                document.getElementsByClassName('box')[0].style.position = "absolute";
+                document.getElementsByClassName('box')[1].style.position = "absolute";
+                document.getElementsByClassName('box')[0].style.width = "0px";
+                document.getElementsByClassName('box')[1].style.width = "0px";
+                document.getElementsByClassName('box')[0].style.bottom = "0";
+                document.getElementsByClassName('box')[1].style.bottom = "0";
+                document.getElementsByClassName('box')[0].style.left = "0";
+                document.getElementsByClassName('box')[1].style.right = "0";
+                document.getElementsByClassName('box')[0].style.height = "100vh";
+                document.getElementsByClassName('box')[1].style.height = "100vh";
                 document.body.style.overflow = "auto";
 
             }
-            function reset()
-            {
-                document.getElementsByClassName('container_jobb')[0].style.position = "relative";
-                document.getElementsByClassName('container_bal')[0].style.position = "relative";
-                document.getElementsByClassName('container_jobb')[0].style.width = "100%";
-                document.getElementsByClassName('container_bal')[0].style.width = "100%";
-                document.getElementsByClassName('container_jobb')[0].style.bottom = "0";
-                document.getElementsByClassName('container_bal')[0].style.bottom = "0";
-                document.getElementsByClassName('container_jobb')[0].style.left = "0";
-                document.getElementsByClassName('container_bal')[0].style.right = "0";
-                document.getElementsByClassName('container_jobb')[0].style.height = "100%";
-                document.getElementsByClassName('container_bal')[0].style.height = "100%";
+function reset()
+{
+                document.getElementsByClassName('box')[1].style.position = "relative";
+                document.getElementsByClassName('box')[0].style.position = "relative";
+                document.getElementsByClassName('box')[1].style.width = "50%";
+                document.getElementsByClassName('box')[0].style.width = "50%";
+                document.getElementsByClassName('box')[1].style.bottom = "0";
+                document.getElementsByClassName('box')[0].style.bottom = "0";
+                document.getElementsByClassName('box')[1].style.left = "0";
+                document.getElementsByClassName('box')[0].style.right = "0";
+                document.getElementsByClassName('box')[1].style.height = "100%";
+                document.getElementsByClassName('box')[0].style.height = "100%";
                 document.body.style.overflow = "hidden";
             }
