e5119eecdb9e9a32767b3b8087285bd9573cf61d
DomaGamerRDG <ress.dominik.ferenc@students.jedlik.eu>
2023-10-24 19:52:25 +0200

    hosjhnhoéi

--- js/js.js
+++ js/js.js
@@ -1,64 +1,12 @@
-function div1Show() {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
-    let div3 = document.getElementById("div3");
-    let div4 = document.getElementById("div4");
-    let div5 = document.getElementById("div5");
-    div1.style.display = "block";
-    div2.style.display = "none";
-    div3.style.display = "none";
-    div4.style.display = "none";
-    div5.style.display = "none";
-  }
-  
-  function div2Show() {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
-    let div3 = document.getElementById("div3");
-    let div4 = document.getElementById("div4");
-    let div5 = document.getElementById("div5");
-    div1.style.display = "none";
-    div2.style.display = "block";
-    div3.style.display = "none";
-    div4.style.display = "none";
-    div5.style.display = "none";
-  }
-  
-  function div3Show() {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
-    let div3 = document.getElementById("div3");
-    let div4 = document.getElementById("div4");
-    let div5 = document.getElementById("div5");
-    div1.style.display = "none";
-    div2.style.display = "none";
-    div3.style.display = "block";
-    div4.style.display = "none";
-    div5.style.display = "none";
-  }
-  
-  function div4Show() {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
-    let div3 = document.getElementById("div3");
-    let div4 = document.getElementById("div4");
-    let div5 = document.getElementById("div5");
-    div1.style.display = "none";
-    div2.style.display = "none";
-    div3.style.display = "none";
-    div4.style.display = "block";
-    div5.style.display = "none";
-  }
+const list = ["div1", "div2", "div3", "div4", "div5"]
 
-  function div5Show() {
-    let div1 = document.getElementById("div1");
-    let div2 = document.getElementById("div2");
-    let div3 = document.getElementById("div3");
-    let div4 = document.getElementById("div4");
-    let div5 = document.getElementById("div5");
-    div1.style.display = "none";
-    div2.style.display = "none";
-    div3.style.display = "none";
-    div4.style.display = "none";
-    div5.style.display = "block";
-  }
+function Show(str) {
+  list.forEach(element => {
+    if(element == str){
+      document.getElementById(element).style.display = "block";
+    }
+    else{
+      document.getElementById(element).style.display = "none";
+    }
+  });
+}
