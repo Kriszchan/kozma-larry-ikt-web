a1c1d48f0fca7593fa46d4b5bc51f1cff1b8ea68
Doma <ress.dominik.ferenc@students.jedlik.eu>
2023-10-25 08:13:19 +0200

    done_végleges

--- js/js.js
+++ js/js.js
@@ -10,3 +10,16 @@ function Show(str) {
     }
   });
 }
+
+const lista = ["div1.", "div2.", "div3.", "div4.", "div5."]
+
+function Show2(str) {
+  lista.forEach(element => {
+    if(element == str){
+      document.getElementById(element).style.display = "block";
+    }
+    else{
+      document.getElementById(element).style.display = "none";
+    }
+  });
+}
